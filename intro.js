a703f875f319aaed824108caeb67d0410342554c
Gbenga Lasisi <lasisig5@gmail.com>
2025-08-21 16:25:16 +0100

    adding into.js to my hithub

---
+++ intro.js
@@ -0,0 +1,164 @@
+ // console.log("Hello world");
+// let a = "God is good";
+// let b = "All the time";
+// console.log(a + b);
+
+
+// operators
+ // arithemetic operators
+  // = -assgnment operator
+  // + addition
+  let num = 34;
+  let num2 = 46;
+  let sum = num + num2;
+  console.log(sum);
+  
+  // - subtraction
+  let difference = num2 - num;
+  console.log(difference);
+
+  // multiplication
+  let product = num * num2;
+  console.log(product);
+
+  //exponential
+  let power = num ** 2;
+  console.log(power);
+
+  //division
+  let divide = num2 / num;
+  console.log(divide);
+
+  // modulus %
+  let mod = 5 % 2;
+  console.log(mod);
+
+  //increment - post - pre
+//   let increment = num++;
+++num;
+//   console.log(increment);
+
+  // decrement 
+  let decrement = num--;
+  console.log("_____________________");
+  console.log(decrement);
+
+  let x = 8;
+  let y = 8;
+  console.log(++x + y++ + 2);
+  
+//   console.log(++x + x++ + 2);
+
+// typeof
+let word = null;
+console.log(typeof word);
+
+//comparison
+
+ // >  greter than, <  less than , >= greater than or equal to , <=  less than or equal to, == loose equal to, === strictly equal to
+ let p = 3;
+ let m = '3';
+ console.log(p >= m);
+ console.log(typeof p, " vs ", typeof m);
+
+ // !, != not equal to, !==
+ let ismarried = true;
+ console.log(!ismarried);
+
+ let r = 40;
+ let t = 44;
+ console.log(r != t);
+
+ // single line comment
+ /**
+  * 
+  * 
+  * 
+  * 
+  * multiline comment
+  */
+ let num4 =2
+ let num3 =4
+ let sum2 = num3 + num
+console.log(sum);
+
+/**
+ * What are the different data types in JavaScript?
+  What’s the difference between == and === in JavaScript?
+
+  What will be the result of 5 + "5" and why?
+
+  write a program that takes two values and tells if they are of the same type and value?
+
+  Given two numbers, write a comparison that checks if the first is greater than the second. Return true or false.
+
+  Write a small script that checks if a number is even or odd using the % operator.
+
+  What is the result of true == 1 and true === 1? Why?
+  
+ */
+console.log("_________________________ class work");
+
+
+  //String; Boolean; Number; Object; Null Bigint; Undefined; Symbol; null
+  // == looselwill affect data will === will not change the data 
+  console.log(5 + "5"); 
+
+  //the first one is string; the second one is number
+  let pt = 4
+  let ty = 7
+  console.log(typeof pt === typeof ty);
+  console.log(pt === ty);
+  
+  console.log(pt > ty);
+  
+
+  console.log(pt % 2 == 0);
+// answer is 1
+console.log(true == 1);
+console.log(true === 1);
+
+
+/**
+ * What data type is returned by typeof null?
+
+What’s the result of typeof NaN?
+
+How do you represent a string in JavaScript?
+
+Is 0 considered truthy or falsy in JavaScript?
+
+What will true + true return?
+
+Write a function that takes two numbers and returns the larger one.
+
+Write a function that takes a number and returns "positive", "negative", or "zero".
+
+Write a line of code that checks if a variable age is not equal to 18.
+
+Create a variable that holds the result of 4 + "2" and explain the result.
+
+What’s the output of this comparison?
+let x = "100";
+let y = 100;
+console.log(x < y);
+ */
+  
+  
+  
+  
+
+
+
+ 
+ 
+ 
+ 
+  
+  
+  
+  
+  
+  
+  
+  
